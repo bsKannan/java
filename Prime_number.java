ebdd1c40eee7d0218332552b7886438cb6e2203f
bsKannan <36512419+bsKannan@users.noreply.github.com>
2018-12-04 10:27:59 +0530

    Update Prime_number.java

--- Prime_number.java
+++ Prime_number.java
@@ -45,3 +45,27 @@ public class Prime {
             System.out.println(num + " is not a prime number.");
     }
 }
+
+
+//Between Two intervals
+
+public class Prime {
+    public static void main(String args[]) {
+    int low =20,high = 50;
+    
+    while (low < high) {
+        boolean flag = false;
+        
+        for(int i = 2;i <= low/2;++i) {
+            if(low % i == 0) {
+                flag = true;
+                break;
+            }
+        }
+            if(!flag)
+                System.out.print(low + "  ");
+                ++low;
+        
+    }
+    }
+}
